ed683c6bd1dedad5b33576134e5916c432755836
PradeepHopkins <Pradeephopkins@gmail.com>
2023-03-16 16:02:25 +0530

    Pom

--- test-output-thread/report.js
+++ test-output-thread/report.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
 CucumberHTML.timelineItems.pushArray([
   {
-    "id": "b92ac442-f573-4526-b24b-bdbf77693b70",
+    "id": "96453890-bd3e-4227-a33d-bac8f56f1dea",
     "feature": "Buy Order Functionality",
     "scenario": "user Purchase the product",
-    "start": 1678961079221,
+    "start": 1678962421262,
     "group": 1,
     "content": "",
     "tags": "",
-    "end": 1678961079368,
+    "end": 1678962434671,
     "className": "passed"
   }
 ]);
